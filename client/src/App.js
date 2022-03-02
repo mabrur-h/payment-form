47f979421edbcc245fa6f78a6b04cbedc5d92809
mabrur-h <mabrur.h11@gmail.com>
2022-03-02 20:17:52 +0500

    update patterns

--- client/src/App.js
+++ client/src/App.js
@@ -4,48 +4,52 @@ import { useState } from "react";
 
 function App() {
     const [values, setValues] = useState({
-        username: "",
-        email: "",
-        birthday: "",
-        password: "",
-        confirmPassword: ""
+        cardNumber: "",
+        expiration: "",
+        cvv: "",
+        amount: ""
     })
 
     const inputs = [
         {
             id: 1,
-            name: "username",
+            name: "cardNumber",
             type: "text",
-            placeholder: "username",
-            label: "username"
+            placeholder: "Card Number",
+            label: "cardNumber",
+            errorMessage: "This field should be only numbers and length 16 symbols",
+            pattern: "^[0-9]{16}$",
+            required: true,
         },
         {
             id: 2,
-            name: "email",
+            name: "expiration",
             type: "text",
             placeholder: "email",
-            label: "email"
+            label: "expiration",
+            pattern: "^(0[1-9]|1[0-2])\\/?([0-9]{4})$",
+            errorMessage: "Expiration date should be in format MM/YYYY",
+            required: true
         },
         {
             id: 3,
-            name: "birthday",
+            name: "cvv",
             type: "text",
-            placeholder: "birthday",
-            label: "birthday"
+            placeholder: "card cvv",
+            label: "cvv",
+            pattern: "^([0-9]{3})$",
+            errorMessage: "CVV should be only numbers and length 3 symbols",
+            required: true
         },
         {
             id: 4,
-            name: "password",
-            type: "password",
-            placeholder: "password",
-            label: "password"
-        },
-        {
-            id: 5,
-            name: "confirmPassword",
-            type: "password",
-            placeholder: "Confirm Password",
-            label: "confirmPassword"
+            name: "amount",
+            type: "text",
+            placeholder: "Amount",
+            label: "amount",
+            errorMessage: "Amount should be only numbers",
+            pattern: `^[0-9]$`,
+            required: true
         }
     ]
 
@@ -62,6 +66,7 @@ function App() {
     return (
         <div className="App">
             <form onSubmit={handleSubmit}>
+                <h1>Payment Form</h1>
                 {inputs.map((input) => (
                     <FormInput
                         key={input.id}
